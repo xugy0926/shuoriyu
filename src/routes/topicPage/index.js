a3217ae8a908aa8af74b99bf7b7846a99b45dff2
young <young_xu@qq.com>
2016-08-29 10:01:21 +0800

    modify

--- src/routes/topicPage/index.js
+++ src/routes/topicPage/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TopicPage from './TopicPage';
 import fetch from '../../core/fetch';
+import { host } from '../../config'
 
 export default {
 
@@ -8,7 +9,7 @@ export default {
 
   async action({path}) {
   	console.log('path = ' + path);
-    const resp = await fetch('http://localhost:3000/api/v1' + path);
+    const resp = await fetch( 'http://' + host + '/api/v1' + path);
     const { success, data } = await resp.json();
     if (!success || !data) throw new Error('Failed to load the topic.');
     return <TopicPage topic={data} />;
