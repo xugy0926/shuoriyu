886a7449837289894c965b9c61c46c9b345d0f58
young <young_xu@qq.com>
2016-09-07 13:07:15 +0800

    modify

--- src/reducers/home.js
+++ src/reducers/home.js
@@ -7,11 +7,17 @@ const initialState = {
 };
 
 export default function (state = initialState, action) {
+
+  const {payload, error, meta = {}, type} = action;
+  const {sequence = {}, tab, id = '0', replyId = '0', userId = '0', content = '', user = {}} = meta;
+
+  if (sequence.type === 'start' || error) {
+    return state;
+  }
+
   switch (action.type) {
 	case types.GET_HOME_TABS:
       let data = action.payload;
-
-      
 	  return {
         ...state,
         tabs: data.tabs,
